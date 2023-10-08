a4189010b8d428e634f90706a1ee9cfd58a04e46
Paul McNamee <paulmc121@gmail.com>
2023-10-08 13:37:51 -0600

    added custom type for the character controller

--- addons/2d_top_down_character_controller/tool.gd
+++ addons/2d_top_down_character_controller/tool.gd
@@ -1,2 +1,10 @@
 @tool
 extends EditorPlugin
+
+
+func _enter_tree():
+	add_custom_type("TopDownPlayerController2D", "CharacterBody2D", preload("2d_top_down_character_controller.gd"), null)
+
+
+func _exit_tree():
+	remove_custom_type("TopDownPlayerController2D")
